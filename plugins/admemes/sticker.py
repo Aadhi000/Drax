4b4facad90b04517b673055649e2321a0a32263c
Aadhi <87893573+Aadhi000@users.noreply.github.com>
2021-12-06 22:13:03 +0530

    Update sticker.py

--- plugins/admemes/sticker.py
+++ plugins/admemes/sticker.py
@@ -4,7 +4,7 @@ from pyrogram import Client, filters
 @Client.on_message(filters.command(["stickerid"]))
 async def stickerid(bot, message):   
     if message.reply_to_message.sticker:
-       await message.reply(f"**Sticker ID is**  \n `{message.reply_to_message.sticker.file_id}` \n \n ** Unique ID is ** \n\n`{message.reply_to_message.sticker.file_unique_id}`", quote=True)
+       await message.reply(f"**𝐒𝐭𝐢𝐜𝐤𝐞𝐫 𝐈𝐃 ››**  \n `{message.reply_to_message.sticker.file_id}` \n \n ** 𝐔𝐧𝐢𝐪𝐮𝐞 𝐈𝐃 ›› ** \n\n`{message.reply_to_message.sticker.file_unique_id}`", quote=True)
     else: 
-       await message.reply("Oops !! Not a sticker file")
+       await message.reply("𝐈𝐭𝐬 𝐍𝐨𝐭 𝐚 𝐒𝐭𝐢𝐜𝐤𝐞𝐫 𝐅𝐢𝐥𝐞")
 
